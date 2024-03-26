007c1100cf243136b0d22c1060b5c54ad4f5e0ce
Viktor Bolotin <viktorbolotinwork@gmail.com>
2024-03-26 15:11:52 +0300

    Добавлены карточки на страницу с возможностью удаления

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/Viktor-Bolotin/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,29 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+for(let i=0; i<initialCards.length; i++) {
+const cardTemplate = document.querySelector('#card-template').content;
+const cardShow = document.querySelector('.places__list');
+
+const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+cardElement.querySelector('.card__image').src = initialCards[i].link;
+cardElement.querySelector('.card__image').alt = initialCards[i].name;
+cardElement.querySelector('.card__title').textContent = initialCards[i].name;
+
+cardShow.append(cardElement);
+}
+
+let DeleteButtonMassive = document.querySelectorAll('.card__delete-button')
+
+let DeleteListener = function(DeleteButton) {
+  DeleteButton.addEventListener('click', function() {
+    let DeleteCard = DeleteButton.closest('.places__item')
+    DeleteCard.remove()
+  })
+}
+
+DeleteButtonMassive.forEach(function(item) {
+  DeleteListener(item);
+})

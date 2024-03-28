10be668ad42e0836d6bcce2280204f9f66ea3af3
Viktor Bolotin <viktorbolotinwork@gmail.com>
2024-03-28 11:13:49 +0300

    Правки после второго ревью

--- scripts/index.js
+++ scripts/index.js
@@ -9,28 +9,28 @@
 // @todo: Вывести карточки на страницу
 
 const cardTemplate = document.querySelector('#card-template').content;
-let cardNumder = 0
-const readyCards = []
 const cardShow = document.querySelector('.places__list');
 
-const createCard = function() {
+function deleteFunc(card) {
+  card.remove()
+}
+
+const createCard = function(item) {
 
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-  cardElement.querySelector('.card__image').src = initialCards[cardNumder].link;
-  cardElement.querySelector('.card__image').alt = initialCards[cardNumder].name;
-  cardElement.querySelector('.card__title').textContent = initialCards[cardNumder].name;
-  cardNumder++;
-  readyCards.push(cardElement);
+  cardElement.querySelector('.card__image').src = item.link;
+  cardElement.querySelector('.card__image').alt = item.name;
+  cardElement.querySelector('.card__title').textContent = item.name;
 
   const deleteButton = cardElement.querySelector('.card__delete-button');
   deleteButton.addEventListener('click', function() {
-    const deleteCard = deleteButton.closest('.places__item')
-    deleteCard.remove()
-  })
+    deleteFunc(cardElement);
+  });
+
+  cardShow.append(cardElement)
 
   return cardElement;
 }
 
 initialCards.forEach(createCard)
 
-cardShow.append(...readyCards)

73ab5fbd731dde1c58e9c96e7e61a2eb11ce4df0
Viktor Bolotin <viktorbolotinwork@gmail.com>
2024-03-29 17:30:09 +0300

    Функция удаления карточки вынесена в отдельную функцию

--- scripts/index.js
+++ scripts/index.js
@@ -10,15 +10,14 @@
 
 const cardTemplate = document.querySelector('#card-template').content;
 const cardShow = document.querySelector('.places__list');
-
-function deleteFunc(card) {
-  card.remove()
-}
-
 let cardElement 
 
-function createCard(item) {
-  
+function deleteFunc(element) {
+  element.closest('.places__item').remove()
+}
+
+function createCard(item, deleteFunc) {
+
   cardElement = cardTemplate.querySelector('.card').cloneNode(true); 
 
   cardElement.querySelector('.card__image').src = item.link;
@@ -26,14 +25,13 @@ function createCard(item) {
   cardElement.querySelector('.card__title').textContent = item.name;
 
   const deleteButton = cardElement.querySelector('.card__delete-button');
-  deleteButton.addEventListener('click', function() {
-    deleteFunc(cardElement);
+  deleteButton.addEventListener('click', function(){
+    deleteFunc(deleteButton)
   });
-
   return cardElement;
 }
 
 initialCards.forEach((card) =>{
-  createCard(card);
+  createCard(card, deleteFunc);
   cardShow.append(cardElement)
 })

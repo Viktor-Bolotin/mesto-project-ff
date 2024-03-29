999343f925e1344614a6bbb8d697dc0078985d73
Viktor Bolotin <viktorbolotinwork@gmail.com>
2024-03-29 17:06:51 +0300

    Карточки не добавляются в цикле создания

--- scripts/index.js
+++ scripts/index.js
@@ -15,9 +15,12 @@ function deleteFunc(card) {
   card.remove()
 }
 
-const createCard = function(item) {
+let cardElement 
 
-  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+function createCard(item) {
+  
+  cardElement = cardTemplate.querySelector('.card').cloneNode(true); 
+
   cardElement.querySelector('.card__image').src = item.link;
   cardElement.querySelector('.card__image').alt = item.name;
   cardElement.querySelector('.card__title').textContent = item.name;
@@ -27,10 +30,10 @@ const createCard = function(item) {
     deleteFunc(cardElement);
   });
 
-  cardShow.append(cardElement)
-
   return cardElement;
 }
 
-initialCards.forEach(createCard)
-
+initialCards.forEach((card) =>{
+  createCard(card);
+  cardShow.append(cardElement)
+})
